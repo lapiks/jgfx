a1de8a5d9451f83c59483200ba147a1c48903bb3
lapiks <dhenin.julien@gmail.com>
2023-10-01 12:27:30 +0200

    Remove use of GLM in example

--- examples/example1/main.cpp
+++ examples/example1/main.cpp
@@ -4,11 +4,6 @@
 #define GLFW_EXPOSE_NATIVE_WIN32
 #include "GLFW/glfw3native.h"
 
-#define GLM_FORCE_RADIANS
-#define GLM_FORCE_DEPTH_ZERO_TO_ONE
-#include <glm/vec4.hpp>
-#include <glm/mat4x4.hpp>
-
 #include <iostream>
 
 #include "jgfx/jgfx.h"
